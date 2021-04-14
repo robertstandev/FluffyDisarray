48d517e28164b4e6314480685645549b8bf97d7a
Robert Stan <58505671+stanrobertdev@users.noreply.github.com>
2021-04-14 10:01:10 +0300

    Create Move.cs

---
+++ Assets/Scripts/Dynamic/Move.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Move : MonoBehaviour
+{
+   [SerializeField]private float moveForce = 2f;
+   [SerializeField]private float runForce = 4f;
+
+   private Rigidbody2D rb;
+   
+   private bool facingRight = true;
+
+   private Vector2 movingRight;
+   private Vector2 movingLeft;
+   private Vector2 runningRight;
+   private Vector2 runningLeft;
+
+   private void Awake(){
+      rb = GetComponent<Rigidbody2D>();
+
+      movingRight = new Vector2(moveForce, 0f);
+      movingLeft = new Vector2((moveForce * -1), 0f);
+
+      runningRight = new Vector2(runForce, 0f);
+      runningLeft = new Vector2((runForce * -1), 0f);
+   }
+
+   public void walk(){
+      if(facingRight){
+        movingRight.y = rb.velocity.y;
+        rb.velocity = movingRight;
+     }else{
+        movingLeft.y = rb.velocity.y;
+        rb.velocity = movingLeft;
+     }
+   }
+
+   public void run(){
+      if(facingRight){
+        runningRight.y = rb.velocity.y;
+        rb.velocity = runningRight;
+     }else{
+        runningLeft.y = rb.velocity.y;
+        rb.velocity = runningLeft;
+     }
+   }
+
+   public void isFacingRight(bool value){
+      this.facingRight = value;
+   }
+}
