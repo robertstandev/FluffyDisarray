6c3c329b7ed884253b13341d1edb4730a15ed086
Robert Stan <58505671+stanrobertdev@users.noreply.github.com>
2021-04-14 10:21:22 +0300

    Create Stamina.cs

---
+++ Assets/Scripts/Dynamic/Stamina.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stamina : MonoBehaviour
+{
+   
+   [SerializeField]private int maximumStamina = 100;
+   private int currentStamina = 100;
+   private Coroutine staminaModifierTimerInstance;
+
+    public int getStamina(){
+       return this.currentStamina;
+   }
+
+    public void setStamina(int value){
+       this.currentStamina = value;
+   }
+
+    public void substractStamina(int amount){
+       this.currentStamina = this.currentStamina >= amount ? this.currentStamina -= amount : 0;
+   }
+
+    public void addStamina(int amount){
+        this.currentStamina = (this.currentStamina + amount) <= maximumStamina ? this.currentStamina += amount : maximumStamina;
+   }
+   
+    private IEnumerator staminaModifierTimer(float interval, System.Action<int> getMethod , int amountPerTick){
+       WaitForSeconds delay = new WaitForSeconds(interval);
+       
+       while(true){
+            yield return delay;
+            getMethod?.Invoke(amountPerTick);
+       }
+   }
+
+   public void stopStaminaModifierTimer(){
+      if(this.staminaModifierTimerInstance != null){
+         StopCoroutine(this.staminaModifierTimerInstance);
+      }
+   }
+
+   public void startStaminaModifierTimer(float interval, System.Action<int> getMethod , int amountPerTick){
+      this.staminaModifierTimerInstance = StartCoroutine(staminaModifierTimer(interval,getMethod,amountPerTick));
+   }
+}
