c5d9bfaef165ccd9e5727efc922aeb0750bb2387
Robert Stan <58505671+stanrobertdev@users.noreply.github.com>
2021-04-15 08:32:46 +0300

    Update CheckGround.cs
    Transition from using the gameobject collider to using BoxCast detection.

--- Assets/Scripts/Dynamic/CheckGround.cs
+++ Assets/Scripts/Dynamic/CheckGround.cs
@@ -2,20 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class CheckGround : MonoBehaviour
 {
-    private bool isGrounded = false;
+    private bool grounded = false;
+    private SpriteRenderer sprite;
+    private RaycastHit2D boxResult;
+    private Vector2 origin;
+    private Vector3 originExtension;
+    private Vector2 boxSize;
 
-    private void OnCollisionEnter2D(Collision2D other) {
-        isGrounded = true;
+    private void Awake() {
+        sprite = GetComponent<SpriteRenderer>();
+        originExtension = new Vector3(0, sprite.bounds.extents.y + boxSize.y, 0);
+        boxSize = new Vector2(0.40f,0.02f);
     }
 
-    private void OnCollisionExit2D(Collision2D other) {
-        isGrounded = false;
+    private void FixedUpdate() {
+        checkGround();
     }
 
-    public bool getIsGroundedState(){
-        return this.isGrounded;
+    private void checkGround(){
+        originExtension.y = sprite.bounds.extents.y + boxSize.y;
+        origin = transform.position - originExtension;
+
+        //Physics2D.BoxCast(origin,size,angleForBoxRotation,directionForBox,distance)
+        boxResult = Physics2D.BoxCast(origin, boxSize, 0, Vector2.down, boxSize.y);
+
+        if(boxResult){
+            grounded = true;
+        }else{
+            grounded = false;
+        }
+    }
+
+    public bool isGrounded(){
+        return this.grounded;
     }
 }
