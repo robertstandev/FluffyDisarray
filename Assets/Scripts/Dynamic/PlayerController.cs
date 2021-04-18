95c07c12745a6d9f4aa08b6565f10a70d46510fe
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-04-18 06:12:23 +0300

    Update PlayerController.cs

--- Assets/Scripts/Dynamic/PlayerController.cs
+++ Assets/Scripts/Dynamic/PlayerController.cs
@@ -30,7 +30,7 @@ public class PlayerController : MonoBehaviour
      private void checkGroundAndModifyStamina(){
           if(canModify && checkGroundComponent.isGrounded()){
                canModify = false;
-               staminaComponent.startStaminaModifierTimer(0.5f, staminaComponent.addStamina, 10);
+               staminaComponent.startStaminaModifierTimer(0.3f, staminaComponent.addStamina, 5);
                jumpComponent.resetJumpCounter();
           }else if(!canModify && !checkGroundComponent.isGrounded()){
                canModify = true;
