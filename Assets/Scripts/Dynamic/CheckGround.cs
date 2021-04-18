73a1b21199374e29e9659649a095920759eb15c3
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-04-18 07:54:55 +0300

    Update CheckGround.cs
    Removed Sprite dependency

--- Assets/Scripts/Dynamic/CheckGround.cs
+++ Assets/Scripts/Dynamic/CheckGround.cs
@@ -2,21 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(SpriteRenderer))]
 public class CheckGround : MonoBehaviour
 {
-    private SpriteRenderer sprite;
     private RaycastHit2D boxResult;
     private Vector3 originExtension;
     private Vector2 boxSize;
 
     private void Awake() {
-        sprite = GetComponent<SpriteRenderer>();
-        originExtension = new Vector3(0, sprite.bounds.extents.y + boxSize.y, 0);
+        originExtension = Vector3.zero;
         boxSize = new Vector2(0.4f,0.02f);
     }
 
-    public bool isGrounded(){
+    public bool isGrounded(SpriteRenderer sprite){
         originExtension.y = sprite.bounds.extents.y + boxSize.y;
         return boxResult = Physics2D.BoxCast(transform.position - originExtension, boxSize, 0, Vector2.down, boxSize.y);
     }
