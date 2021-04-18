1abdd7c49b223377197b2662bdaeded4c64a289b
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-04-18 07:43:55 +0300

    Update Jump.cs

--- Assets/Scripts/Dynamic/Jump.cs
+++ Assets/Scripts/Dynamic/Jump.cs
@@ -2,10 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
 public class Jump : MonoBehaviour
 {
-    private Rigidbody2D rb;
     [SerializeField]private float jumpForce = 10f;
     [SerializeField]private int maxNrOfJumps = 2;
     private int jumpCounter = 0;
@@ -13,11 +11,10 @@ public class Jump : MonoBehaviour
 
 
     private void Awake(){
-        rb = GetComponent<Rigidbody2D>();
         jumpVelocity = new Vector2(0f, jumpForce);
     }
     
-    public void jump(){
+    public void jump(Rigidbody2D rb){
         if(canJump()){
             jumpCounter += 1;
             rb.velocity = jumpVelocity;
