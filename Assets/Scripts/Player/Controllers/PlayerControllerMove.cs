6ff9844890555fc28ccc005155f0e0cfa2ac4138
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-04-27 12:51:06 +0300

    Update PlayerControllerMove.cs

--- Assets/Scripts/Player/Controllers/PlayerControllerMove.cs
+++ Assets/Scripts/Player/Controllers/PlayerControllerMove.cs
@@ -3,13 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Movement))]
 public class PlayerControllerMove : MonoBehaviour
 {
     private InputAction movementInput;
     private Movement movementComponent;
 
-    private void Awake() {
+    private void Awake()
+    {
         movementInput = GetComponent<IPlayerInput>().getMovementInput;
+        movementInput.performed += context => OnMove(context);
+        movementInput.canceled += context => OnMove(context);
+
         movementComponent = GetComponent<Movement>();
     }
+
+    private void OnEnable()
+    {
+        movementInput.Enable();
+        movementComponent.enabled = true;
+    }
+    private void OnDisable()
+    {
+        movementInput.Disable();
+        movementComponent.enabled = false;
+    }
+
+    private void OnMove(InputAction.CallbackContext context) {  movementComponent.move(context.ReadValue<float>()); }
 }
