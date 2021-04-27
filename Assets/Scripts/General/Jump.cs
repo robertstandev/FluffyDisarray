dc86602f2cb84f7f0c2db9be07e36feb36e07ab1
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-04-27 12:50:19 +0300

    Update Jump.cs

--- Assets/Scripts/General/Jump.cs
+++ Assets/Scripts/General/Jump.cs
@@ -2,31 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Jump : MonoBehaviour
 {
+    private Rigidbody2D rb;
     [SerializeField]private float jumpForce = 10f;
     [SerializeField]private int maxNrOfJumps = 2;
     private int jumpCounter = 0;
-    private Vector2 jumpVelocity;
+    private Vector2 jumpVelocity = Vector2.zero;
 
-    private void Awake(){
-        jumpVelocity = new Vector2(0f, jumpForce);
-    }
-    
-    public void jump(Rigidbody2D rb){
-        if(canJump()){
+    private void Awake(){ rb = GetComponent<Rigidbody2D>(); }
+
+    public bool canJump() { return jumpCounter < maxNrOfJumps; }    
+
+    public void jump(Stamina staminaComponent, int staminaToConsume){
+        if(canJump() && (staminaComponent.getStamina() >= staminaToConsume)){
             jumpCounter += 1;
+
+            jumpVelocity.y = jumpForce;
             rb.velocity = jumpVelocity;
         }
     }
     
-    public bool canJump(){
-       return jumpCounter < maxNrOfJumps;
-    }
-
-    public int getJumpCounter(){
-        return this.jumpCounter;
-    }
+    public int getJumpCounter() { return this.jumpCounter; }
 
     public void setJumpCounter(int value){
         if(value <= maxNrOfJumps){
