f782f9c46021ce4a2622b6db974c25853d56ef3c
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-04-27 18:36:06 +0300

    Update Movement.cs

--- Assets/Scripts/General/Movement.cs
+++ Assets/Scripts/General/Movement.cs
@@ -2,44 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
-[RequireComponent(typeof(SpriteRenderer))]
 public class Movement : MonoBehaviour
 {
    [SerializeField]private float moveForce = 2f;
    [SerializeField]private float runForce = 4f;
 
-    private Rigidbody2D rb;
-    private SpriteRenderer mySpriteRenderer;
-
    private Vector2 movingVector2 = Vector2.zero;
 
-   private void Awake()
+   public void move(SpriteRenderer mySpriteRenderer, float movingFloat)
    {
-      rb = GetComponent<Rigidbody2D>();
-      mySpriteRenderer = GetComponent<SpriteRenderer>();
+      checkFlip(mySpriteRenderer, movingFloat);
+      movingVector2.x = movingFloat * moveForce;
    }
 
-   private void FixedUpdate() {
+   public void run(SpriteRenderer mySpriteRenderer, float movingFloat)
+   {
+      checkFlip(mySpriteRenderer, movingFloat);
+      movingVector2.x = movingFloat * runForce;
+   }
+
+   public void moveCharacter(Rigidbody2D rb)
+   {
       movingVector2.y = rb.velocity.y;
       rb.velocity = movingVector2;
    }
 
-   public void move(float movingFloat){
-      checkFlip(movingFloat);
-      movingVector2.x = movingFloat * moveForce;
-   }
-
-   public void run(float movingFloat){
-      checkFlip(movingFloat);
-      movingVector2.x = movingFloat * runForce;
-   }
-
-   public void checkFlip(float movingFloat){
+   public void checkFlip(SpriteRenderer mySpriteRenderer, float movingFloat)
+   {
       if(movingFloat > 0)
       {
          mySpriteRenderer.flipX = false;
-      }else if(movingFloat < 0)
+      }
+      else if(movingFloat < 0)
       {
          mySpriteRenderer.flipX = true;
       }
