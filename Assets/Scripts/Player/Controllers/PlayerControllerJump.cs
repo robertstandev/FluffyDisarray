7d1757a0f50025ad1af66e9b9228a6c517fe3e6a
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-04-27 18:36:42 +0300

    Update PlayerControllerJump.cs

--- Assets/Scripts/Player/Controllers/PlayerControllerJump.cs
+++ Assets/Scripts/Player/Controllers/PlayerControllerJump.cs
@@ -5,11 +5,13 @@ using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Jump))]
 [RequireComponent(typeof(Stamina))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerControllerJump : MonoBehaviour
 {
     private InputAction jumpInput;
     private Jump jumpComponent;
     private Stamina staminaComponent;
+    private Rigidbody2D rb;
 
     private void Awake() {
         jumpInput = GetComponent<IPlayerInput>().getJumpInput;
@@ -17,10 +19,11 @@ public class PlayerControllerJump : MonoBehaviour
 
         jumpComponent = GetComponent<Jump>();
         staminaComponent = GetComponent<Stamina>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void OnEnable() { jumpInput.Enable(); }
     private void OnDisable() { jumpInput.Disable(); }
 
-    private void OnJump() { jumpComponent.jump(staminaComponent, 10); }
+    private void OnJump() { jumpComponent.jump(rb, staminaComponent, 10); }
 }
