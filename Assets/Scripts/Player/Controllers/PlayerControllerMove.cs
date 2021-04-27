94c90f9d9d8974d9fda2d2c0484662b3306e4cc3
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-04-27 18:37:29 +0300

    Update PlayerControllerMove.cs

--- Assets/Scripts/Player/Controllers/PlayerControllerMove.cs
+++ Assets/Scripts/Player/Controllers/PlayerControllerMove.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Movement))]
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class PlayerControllerMove : MonoBehaviour
 {
     private InputAction movementInput;
     private Movement movementComponent;
+    private Rigidbody2D rb;
+    private SpriteRenderer mySpriteRenderer;
 
     private void Awake()
     {
@@ -16,18 +20,14 @@ public class PlayerControllerMove : MonoBehaviour
         movementInput.canceled += context => OnMove(context);
 
         movementComponent = GetComponent<Movement>();
+        rb = GetComponent<Rigidbody2D>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    private void OnEnable()
-    {
-        movementInput.Enable();
-        movementComponent.enabled = true;
-    }
-    private void OnDisable()
-    {
-        movementInput.Disable();
-        movementComponent.enabled = false;
-    }
+    private void OnEnable() { movementInput.Enable(); }
+    private void OnDisable() { movementInput.Disable(); }
 
-    private void OnMove(InputAction.CallbackContext context) {  movementComponent.move(context.ReadValue<float>()); }
+    private void OnMove(InputAction.CallbackContext context) {  movementComponent.move(mySpriteRenderer, context.ReadValue<float>()); }
+
+    private void FixedUpdate() { movementComponent.moveCharacter(rb); }
 }
