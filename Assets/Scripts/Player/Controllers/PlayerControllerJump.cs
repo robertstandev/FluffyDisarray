2a81913377f87a38f749bff14334371a5118443d
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-04-27 12:51:21 +0300

    Update PlayerControllerJump.cs

--- Assets/Scripts/Player/Controllers/PlayerControllerJump.cs
+++ Assets/Scripts/Player/Controllers/PlayerControllerJump.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Jump))]
+[RequireComponent(typeof(Stamina))]
 public class PlayerControllerJump : MonoBehaviour
 {
     private InputAction jumpInput;
     private Jump jumpComponent;
+    private Stamina staminaComponent;
+
     private void Awake() {
         jumpInput = GetComponent<IPlayerInput>().getJumpInput;
+        jumpInput.performed += context => OnJump();
+
         jumpComponent = GetComponent<Jump>();
+        staminaComponent = GetComponent<Stamina>();
     }
+
+    private void OnEnable() { jumpInput.Enable(); }
+    private void OnDisable() { jumpInput.Disable(); }
+
+    private void OnJump() { jumpComponent.jump(staminaComponent, 10); }
 }
