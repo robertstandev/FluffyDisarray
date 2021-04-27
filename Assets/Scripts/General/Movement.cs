6412889b104a3b9926109ff9472c7a6db2615e62
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-04-27 12:49:50 +0300

    Update Movement.cs

--- Assets/Scripts/General/Movement.cs
+++ Assets/Scripts/General/Movement.cs
@@ -2,50 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class Movement : MonoBehaviour
 {
    [SerializeField]private float moveForce = 2f;
    [SerializeField]private float runForce = 4f;
-   
-   private bool facingRight = true;
-   private Vector3 facingRightOrientation;
-   private Vector3 facingLeftOrientation;
 
-   private Vector2 movingRight;
-   private Vector2 movingLeft;
-   private Vector2 runningRight;
-   private Vector2 runningLeft;
+    private Rigidbody2D rb;
+    private SpriteRenderer mySpriteRenderer;
 
-   private void Awake(){
-      movingRight = new Vector2(moveForce, 0f);
-      movingLeft = new Vector2((moveForce * -1), 0f);
+   private Vector2 movingVector2 = Vector2.zero;
 
-      runningRight = new Vector2(runForce, 0f);
-      runningLeft = new Vector2((runForce * -1), 0f);
-
-      facingRightOrientation = transform.localScale;
-      facingLeftOrientation = new Vector3(transform.localScale.x * -1, transform.localScale.y , transform.localScale.z);
+   private void Awake()
+   {
+      rb = GetComponent<Rigidbody2D>();
+      mySpriteRenderer = GetComponent<SpriteRenderer>();
    }
 
-   public void move(Rigidbody2D rb){
-      velocityModifier(rb, facingRight ? movingRight : movingLeft);
+   private void FixedUpdate() {
+      movingVector2.y = rb.velocity.y;
+      rb.velocity = movingVector2;
    }
 
-   public void run(Rigidbody2D rb){
-      velocityModifier(rb, facingRight ? runningRight : runningLeft);
+   public void move(float movingFloat){
+      checkFlip(movingFloat);
+      movingVector2.x = movingFloat * moveForce;
    }
 
-   private void velocityModifier(Rigidbody2D rb, Vector2 movementTypeAndDirection){
-      movementTypeAndDirection.y = rb.velocity.y;
-      rb.velocity = movementTypeAndDirection;
+   public void run(float movingFloat){
+      checkFlip(movingFloat);
+      movingVector2.x = movingFloat * runForce;
    }
 
-   public bool isFacingRight(){
-      return this.facingRight;
-   }
-
-   public void flip(){
-      this.facingRight = !facingRight;
-      transform.localScale = facingRight ? facingRightOrientation : facingLeftOrientation;
+   public void checkFlip(float movingFloat){
+      if(movingFloat > 0)
+      {
+         mySpriteRenderer.flipX = false;
+      }else if(movingFloat < 0)
+      {
+         mySpriteRenderer.flipX = true;
+      }
    }
 }
