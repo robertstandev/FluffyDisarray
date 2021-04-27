04f6d83a3c33dd4bf0abb5a3255e54284f7f1b22
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-04-27 11:01:56 +0300

    Update PlayerControllerMove.cs

--- Assets/Scripts/Player/Controllers/PlayerControllerMove.cs
+++ Assets/Scripts/Player/Controllers/PlayerControllerMove.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerControllerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private InputAction movementInput;
+    private Movement movementComponent;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Awake() {
+        movementInput = GetComponent<IPlayerInput>().getMovementInput;
+        movementComponent = GetComponent<Movement>();
     }
 }
