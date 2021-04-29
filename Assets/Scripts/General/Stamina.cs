0ee08b3a551b85f1f70c427f68ad9df61169788b
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-04-29 12:47:57 +0300

    Update Stamina.cs

--- Assets/Scripts/General/Stamina.cs
+++ Assets/Scripts/General/Stamina.cs
@@ -13,27 +13,35 @@ public class Stamina : MonoBehaviour
 
    public void addStamina(int amount) { this.currentStamina = (this.currentStamina + amount) <= 100 ? this.currentStamina += amount : 100; }
    
-   private IEnumerator staminaModifierTimer(float interval, System.Action<int> getMethod , int amountPerTick){
+   private IEnumerator staminaModifierTimer(float interval, System.Action<int> getMethod , int amountPerTick)
+   {
       WaitForSeconds delay = new WaitForSeconds(interval);
        
-      while(true){
+      while(true)
+      {
             yield return delay;
 
             getMethod?.Invoke(amountPerTick);
 
-            if(((getMethod == addStamina) && (currentStamina == 100)) || ((getMethod == substractStamina) && (currentStamina == 0))){
+            if(((getMethod == addStamina) && (currentStamina == 100)) || ((getMethod == substractStamina) && (currentStamina == 0)))
+            {
                stopStaminaModifierTimer();
             }
       }
    }
 
    public void stopStaminaModifierTimer(){
-      if(this.staminaModifierTimerInstance != null){
+      if(this.staminaModifierTimerInstance != null)
+      {
          StopCoroutine(this.staminaModifierTimerInstance);
+         staminaModifierTimerInstance = null;
       }
    }
 
    public void startStaminaModifierTimer(float interval, System.Action<int> getMethod , int amountPerTick){
+      if(staminaModifierTimerInstance == null)
+      {
       this.staminaModifierTimerInstance = StartCoroutine(staminaModifierTimer(interval,getMethod,amountPerTick));
+      }
    }
 }
