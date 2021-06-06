eebe50adcb8f67fb271ee3dcc97532d8974e3968
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-06-06 12:21:17 +0300

    Add files via upload

---
+++ Assets/Scripts/General/AnimationsManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationsManager : MonoBehaviour
+{
+    private Animation currentAnimation;
+
+    //aici se stocheaza informatiile pentru fiecare sprite in parte pentru a nu mai fi recalculate====================================================
+    //format (int, Vector2, Vector2, Vector2) = (spriteInstanceID, colliderSize , groundCheckBoxCastSize, frontCheckBoxCastSize)
+    private (int, Vector2, Vector2, Vector2)[] spritesInformationArray;
+    //verifica daca spriteul e deja in lista si daca e atunci:
+    //Vector2 colliderSize = GetComponent<BoxCollider2D>().size = spritesInformationArray[0].Item2; //0 = index la spriteInstanceID , facut alta metoda care da return cu spriteInstanceID
+    //Vector2 groundCheckBoxCastSize = GetComponent<BoxCollider2D>().size = spritesInformationArray[0].Item3;
+    // etc
+    //instance IDUL mySpriteRenderer.sprite.GetInstanceID()
+    //================================================================================================================================================
+    public void changeAnimation(Animation animationToPlay)
+    {
+        if(this.currentAnimation != animationToPlay)
+        {
+            this.currentAnimation = animationToPlay;
+            
+            animationToPlay.Play();
+
+            stopWhenFinished(animationToPlay.clip.length);
+        }
+    }
+
+    private IEnumerator stopWhenFinished(float timeToWait)
+    {
+        yield return new WaitForSeconds(timeToWait);
+        StopCoroutine("stopWhenFinished");
+        endOfClip();
+    }
+
+    private void endOfClip()
+    {
+
+    }
+}
--- Assets/Scripts/General/CheckSurroundings.cs
+++ Assets/Scripts/General/CheckSurroundings.cs
@@ -1,18 +1,77 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CheckSurroundings : MonoBehaviour
 {
-    private RaycastHit2D groundCheckBoxTrigger;
-    private Vector2 positionWhereBoxCastStarts;
-    private Vector2 groundCheckBoxSize = new Vector2(0f, 0.025f);
+    private RaycastHit2D raycastHit2D;
+    private Vector2 boxCastPosition;
+    private Vector2 boxCastSize;
+    private Vector2 boxCastDirection;
+    private Vector2 temporarySpriteGroundData;
 
-    public bool isGrounded(SpriteRenderer sprite){
-        groundCheckBoxSize.x = sprite.bounds.size.x;
-        positionWhereBoxCastStarts = transform.position;
-        positionWhereBoxCastStarts.y -= sprite.bounds.extents.y + 0.05f;
+    private bool isBoxCastColliding()
+    {
+        return this.raycastHit2D = Physics2D.BoxCast(this.boxCastPosition, this.boxCastSize, 0, this.boxCastDirection, this.boxCastSize.y);
+    }
+
+    public bool isGrounded(SpriteRenderer spriteRenderer)
+    {
+        temporarySpriteGroundData = getGroundContactData(spriteRenderer);
+
+        this.boxCastSize.x = temporarySpriteGroundData.y;
+        this.boxCastSize.y = 0.025f;
+
+        this.boxCastPosition.x = transform.position.x - spriteRenderer.bounds.extents.x + (temporarySpriteGroundData.y / 2) + temporarySpriteGroundData.x;
+        this.boxCastPosition.y = transform.position.y - spriteRenderer.bounds.extents.y - 0.05f;
+
+        this.boxCastDirection = Vector2.down;
+
+        return isBoxCastColliding();
+    }
+
+    public Vector2 getGroundContactData(SpriteRenderer spriteRenderer)
+    {
+        Color[] pixels = spriteRenderer.sprite.texture.GetPixels((int)spriteRenderer.sprite.textureRect.x , (int)spriteRenderer.sprite.textureRect.y , (int)spriteRenderer.sprite.textureRect.width , 1);        
+
+        float spriteStartingX = 0;
+        float spriteEndX = 0;
+
+        for(int i = 0; i < pixels.Length; i++)
+        {
+            if(pixels[i].a > 0.1f)
+            {
+                if(spriteStartingX == 0)
+                {
+                    spriteStartingX = i;
+                }
+                if(spriteEndX < i)
+                {
+                    spriteEndX = i;
+                }
+            }
+        }
+
+        return new Vector2(spriteStartingX / 100, (spriteEndX - spriteStartingX + 1) / 100);
+    }
+
+    public bool canGrabLedge(SpriteRenderer spriteRenderer)
+    {
+        this.boxCastSize.x = 0.1f;
+        this.boxCastSize.y = spriteRenderer.bounds.size.y - 0.2f;
         
-        return groundCheckBoxTrigger = Physics2D.BoxCast(positionWhereBoxCastStarts, groundCheckBoxSize, 0, Vector2.down, groundCheckBoxSize.y);
+        if(!spriteRenderer.flipX)
+        {
+            this.boxCastPosition.x = transform.position.x + spriteRenderer.bounds.extents.x + 0.1f;
+        }
+        else
+        {
+            this.boxCastPosition.x = transform.position.x - spriteRenderer.bounds.extents.x - 0.1f;
+        }
+        this.boxCastPosition.y = transform.position.y + 0.1f;
+
+        this.boxCastDirection = Vector2.zero;
+
+        return isBoxCastColliding();
     }
 }
