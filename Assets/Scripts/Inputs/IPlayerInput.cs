9109f47635e6e95cd47b8391d859a12fa5d04d4f
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-10-10 16:56:42 +0300

    Add files via upload

---
+++ Assets/Scripts/Inputs/IPlayerInput.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public interface IPlayerInput
+{
+    InputAction getMovementInput { get; }
+    InputAction getJumpInput { get; }
+    InputAction getUpInput { get; }
+    InputAction getDownInput { get; }
+    InputAction getProjectileInput { get; }
+}
---
+++ Assets/Scripts/Inputs/PCPlayerInput.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PCPlayerInput : MonoBehaviour , IPlayerInput
+{
+    [SerializeField]private InputAction movementInput;
+    [SerializeField]private InputAction upInput;
+    [SerializeField]private InputAction downInput;
+    [SerializeField]private InputAction jumpInput;
+    [SerializeField]private InputAction projectileInput;
+
+    public InputAction getMovementInput { get { return this.movementInput; } }
+    public InputAction getJumpInput { get { return this.jumpInput; } }
+    public InputAction getUpInput { get { return this.upInput; } }
+    public InputAction getDownInput { get { return this.downInput; } }
+    public InputAction getProjectileInput { get { return this.projectileInput; } }
+}
