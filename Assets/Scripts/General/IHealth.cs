b7978d91138be44d27a484f224cec73b75450591
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-10-11 07:55:21 +0300

    Delete IHealth.cs

--- Assets/Scripts/General/IHealth.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IHealth
-{
-    void addHealth(int value);
-    void substractHealth(int value);
-}
