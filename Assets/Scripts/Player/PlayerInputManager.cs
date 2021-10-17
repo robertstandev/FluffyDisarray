8a84b2974ed70dee0ca65e0e903b6eec2e85a0ad
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-10-17 18:52:07 +0300

    Add files via upload

--- Assets/Scripts/Player/PlayerInputManager.cs
+++ Assets/Scripts/Player/PlayerInputManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerInputManager : MonoBehaviour , IPlayerInput
+public class PlayerInputManager : MonoBehaviour
 {
     [SerializeField]private InputAction movementInput;
     [SerializeField]private InputAction upInput;
