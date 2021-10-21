601372e30d41ba58dc9dd19403fc13ba9c620d7c
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-10-21 23:58:34 +0300

    Update CollectibleInvisibilityEffect.cs

--- Assets/Scripts/Collectibles/Effects/CollectibleInvisibilityEffect.cs
+++ Assets/Scripts/Collectibles/Effects/CollectibleInvisibilityEffect.cs
@@ -6,10 +6,12 @@ public class CollectibleInvisibilityEffect : MonoBehaviour
 {
     private List<Transform> listOfGameObjectsToChangeLayer = new List<Transform>();
     private List<string> ignoreInvisibilityForThisObjects = new List<string>();
-
-    private void OnEnable()
+    private void Awake()
     {
         this.listOfGameObjectsToChangeLayer.AddRange(transform.parent.GetComponentsInChildren<Transform>(true));
+    }
+    private void OnEnable()
+    {
         setLayer(1);
     }
 
