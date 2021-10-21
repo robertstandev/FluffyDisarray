00f8f1d4228783c498fa6a5e8feec8f27037023d
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-10-21 23:52:09 +0300

    Auto repopulate list Functionality > Performance
    Sacrifice performance for functionality since new collectibles/items might have spawned after the script has started.

--- Assets/Scripts/Collectibles/Effects/CollectibleInvisibilityEffect.cs
+++ Assets/Scripts/Collectibles/Effects/CollectibleInvisibilityEffect.cs
@@ -6,12 +6,10 @@ public class CollectibleInvisibilityEffect : MonoBehaviour
 {
     private List<Transform> listOfGameObjectsToChangeLayer = new List<Transform>();
     private List<string> ignoreInvisibilityForThisObjects = new List<string>();
-    private void Awake()
-    {
-        this.listOfGameObjectsToChangeLayer.AddRange(transform.parent.GetComponentsInChildren<Transform>(true));
-    }
+
     private void OnEnable()
     {
+        this.listOfGameObjectsToChangeLayer.AddRange(transform.parent.GetComponentsInChildren<Transform>(true));
         setLayer(1);
     }
 
