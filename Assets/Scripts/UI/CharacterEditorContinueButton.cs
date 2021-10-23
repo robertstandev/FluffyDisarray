65d6a9c56b293b4117ba719dba9059ed36dde5cf
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-10-23 22:28:21 +0300

    Update

--- Assets/Scripts/UI/CharacterEditorContinueButton.cs
+++ Assets/Scripts/UI/CharacterEditorContinueButton.cs
@@ -9,11 +9,12 @@ public class CharacterEditorContinueButton : MonoBehaviour, IPointerDownHandler
     [SerializeField]private MapCharacterManager mapCharacterManagerGameObject;
     [SerializeField]private Text characterTypeText;
     [SerializeField]private Image characterColorImage;
+    [SerializeField]private CharacterEditorProjectilePicker characterProjectilePrefab;//fac un script care are 2 array : 1 cu imagini si 1 cu prefaburi de projectile , fiecare cu indexul celuilalt
     [SerializeField]private CharacterEditorKeyBindingManager keyBindingManager;
     public void OnPointerDown(PointerEventData eventData)
     {
        Destroy(GetComponent<HideShowTouch>());
-       //this.mapCharacterManagerGameObject.setCharacterDetails(this.characterTypeText.text, this.characterColorImage.color, this.keyBindingManager)
+       //this.mapCharacterManagerGameObject.setCharacterDetails(this.characterTypeText.text, this.characterColorImage.color, this.characterProjectilePrefab, this.keyBindingManager)
        //create on mapCharacterManager lists with all of this values like 
        //List<string>...Add(from parameter characterTypeText.text)
        //List<Color32>...Add(from parameter characterColorImage.color)
