641f90980026575b47d188f6bc79e0259b863484
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-10-23 22:13:59 +0300

    Update Character Editor

--- Assets/Scripts/UI/CharacterEditorContinueButton.cs
+++ Assets/Scripts/UI/CharacterEditorContinueButton.cs
@@ -13,6 +13,10 @@ public class CharacterEditorContinueButton : MonoBehaviour, IPointerDownHandler
     public void OnPointerDown(PointerEventData eventData)
     {
        Destroy(GetComponent<HideShowTouch>());
-       //this.mapCharacterManagerGameObject.setCharacterDetails
+       //this.mapCharacterManagerGameObject.setCharacterDetails(this.characterTypeText.text, this.characterColorImage.color, this.keyBindingManager)
+       //create on mapCharacterManager lists with all of this values like 
+       //List<string>...Add(from parameter characterTypeText.text)
+       //List<Color32>...Add(from parameter characterColorImage.color)
+       //List<CharacterEditorKeyBindingManager>.Add(from parameter new CharacterEditorKeyBindingManager from parameter (altfel toate le vor lua pe a lu ala))
     }
 }
