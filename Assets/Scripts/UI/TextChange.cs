a1f710340062474d281fb6b5055a2145f325192b
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-10-23 19:20:35 +0300

    Character Editor Update

---
+++ Assets/Scripts/UI/TextChange.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class TextChange : MonoBehaviour, IPointerDownHandler
+{
+    [SerializeField]private Text[] textObjectsToChangeStringsTo;
+    [SerializeField]private string[] stringsToSwapBetween;
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        changeString();
+    }
+
+    private void changeString()
+    {
+        Debug.Log(this.textObjectsToChangeStringsTo[0].text);
+        for(int i = 0 ; i < this.textObjectsToChangeStringsTo.Length; i++)
+        {
+            this.textObjectsToChangeStringsTo[i].text = getNextString(this.textObjectsToChangeStringsTo[i].text);
+        }
+    }
+
+    private string getNextString(string currentString)
+    {
+        for(int i = 0 ; i < this.stringsToSwapBetween.Length ; i ++)
+        {
+            if(this.stringsToSwapBetween[i].Equals(currentString))
+            {
+                return i.Equals(this.stringsToSwapBetween.Length - 1) ? this.stringsToSwapBetween[i - 1] : this.stringsToSwapBetween[i + 1];
+            }
+        }
+
+        return "Error Getting String";
+    }
+}
