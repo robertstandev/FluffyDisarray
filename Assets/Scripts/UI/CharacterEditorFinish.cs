b26f4da1f4df6c6590bb3046e56ba1ceb8c6bfbe
Robert Stan <58505671+robertstandev@users.noreply.github.com>
2021-10-24 08:42:03 +0300

    Updata Character Editor

--- Assets/Scripts/UI/CharacterEditorFinish.cs
+++ Assets/Scripts/UI/CharacterEditorFinish.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class CharacterEditorFinish : MonoBehaviour
 {
     [SerializeField]private MapCharacterManager mapCharacterManagerScript;
-    [SerializeField]private CharacterEditorContinueButton characterEditorContinueButtonScript;
 
     private void OnDisable()
     {
