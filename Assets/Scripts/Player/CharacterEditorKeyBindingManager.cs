d6ae975ffc72498153e0769da06bfd28d8e3baa7
Robert Stan <robertstan1991@gmail.com>
2021-10-30 00:44:14 +0300

    Update CharacterEditorKeyBindingManager.cs

--- Assets/Scripts/Player/CharacterEditorKeyBindingManager.cs
+++ Assets/Scripts/Player/CharacterEditorKeyBindingManager.cs
@@ -20,4 +20,12 @@ public class CharacterEditorKeyBindingManager : MonoBehaviour
     public void setProjectileInput(InputAction inputAction) { this.projectileInput = inputAction; }
     public void setSlashInput(InputAction inputAction) { this.slashInput = inputAction; }
     public void setMenuInput(InputAction inputAction) { this.menuInput = inputAction; }
+
+    public InputAction getMovementInput { get { return this.movementInput; } }
+    public InputAction getJumpInput { get { return this.jumpInput; } }
+    public InputAction getUpInput { get { return this.upInput; } }
+    public InputAction getDownInput { get { return this.downInput; } }
+    public InputAction getProjectileInput { get { return this.projectileInput; } }
+    public InputAction getSlashInput { get { return this.slashInput; } }
+    public InputAction getMenuInput { get { return this.menuInput; } }
 }
