2417ffc2667e0c4e1b696403f8e724feb6586411
Robert Stan <robertstan1991@gmail.com>
2021-10-30 00:42:32 +0300

    Key Rebinding System

--- Assets/Scripts/Player/CharacterEditorKeyBindingManager.cs
+++ Assets/Scripts/Player/CharacterEditorKeyBindingManager.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CharacterEditorKeyBindingManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]private InputAction movementInput;
+    [SerializeField]private InputAction upInput;
+    [SerializeField]private InputAction downInput;
+    [SerializeField]private InputAction jumpInput;
+    [SerializeField]private InputAction projectileInput;
+    [SerializeField]private InputAction slashInput;
+    [SerializeField]private InputAction menuInput;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public void setMovementInput(InputAction inputAction) { this.movementInput = inputAction; }
+    public void setUpInput(InputAction inputAction) { this.upInput = inputAction; }
+    public void setDownInput(InputAction inputAction) { this.downInput = inputAction; }
+    public void setJumpInput(InputAction inputAction) { this.jumpInput = inputAction; }
+    public void setProjectileInput(InputAction inputAction) { this.projectileInput = inputAction; }
+    public void setSlashInput(InputAction inputAction) { this.slashInput = inputAction; }
+    public void setMenuInput(InputAction inputAction) { this.menuInput = inputAction; }
 }
---
+++ Assets/Scripts/Player/InputKeyRebinder.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class InputKeyRebinder : MonoBehaviour, IPointerDownHandler
+{
+    private enum typesOfInputs {Button , Axis};
+    [SerializeField]private typesOfInputs typeOfInput = typesOfInputs.Button;
+    [SerializeField]private CharacterEditorKeyBindingManager keyBindingManager;
+    private enum inputList { movementInput, downInput, upInput, jumpInput, projectileInput, slashInput, menuInput};
+    [SerializeField]private inputList inputToReplace = inputList.movementInput;
+    [SerializeField]private Text textComponent;
+    private InputAction actionInput = new InputAction();
+
+    private string originalText;
+
+    private InputActionRebindingExtensions.RebindingOperation rebindingOperation;
+
+    public void OnPointerDown(PointerEventData eventData) { getUserInput(); }
+
+    private void getUserInput()
+    {
+        this.originalText  = this.textComponent.text;
+        if(this.typeOfInput.Equals(typesOfInputs.Button))
+        {
+            this.textComponent.text = "Press Key";
+            configureButtonInput();
+        }
+        else
+        {
+            this.textComponent.text = "Press Key(s)";
+            configureAxisInput();
+        }
+    }
+
+    private void configureAxisInput()
+    {
+        this.actionInput.AddCompositeBinding("Axis") // Or just "Axis"
+        .With("Positive", "<Gamepad>/rightTrigger")
+        .With("Negative", "<Gamepad>/leftTrigger");
+
+        this.rebindingOperation = actionInput.PerformInteractiveRebinding(0)
+        .WithRebindAddingNewBinding()
+        .WithControlsExcluding("Mouse")
+        .OnMatchWaitForAnother(0.1f)
+        .OnCancel(operation => rebindCanceled())
+        .Start();
+
+        this.rebindingOperation = actionInput.PerformInteractiveRebinding(1)
+        .WithRebindAddingNewBinding()
+        .WithControlsExcluding("Mouse")
+        .OnMatchWaitForAnother(0.1f)
+        .OnCancel(operation => rebindCanceled())
+        .Start();
+  
+        rebindComplete();
+    }
+
+    private void configureButtonInput()
+    {
+        this.rebindingOperation = actionInput.PerformInteractiveRebinding()
+        .WithRebindAddingNewBinding()
+        .WithControlsExcluding("Mouse")
+        .OnMatchWaitForAnother(0.1f)
+        .OnComplete(operation => rebindComplete())
+        .OnCancel(operation => rebindCanceled())
+        .Start();
+    }
+
+    private void rebindComplete()
+    {
+        this.textComponent.text = InputControlPath.ToHumanReadableString(this.actionInput.bindings[0].effectivePath, InputControlPath.HumanReadableStringOptions.OmitDevice);
+        setKeyBinding();
+        this.rebindingOperation.Dispose();
+    }
+
+    private void rebindCanceled()
+    {
+        this.textComponent.text = this.originalText;
+    }
+
+    private void setKeyBinding()
+    {
+       if(this.inputToReplace.Equals(inputList.movementInput))
+       {
+           this.keyBindingManager.setMovementInput(this.actionInput);
+       }
+       else if(this.inputToReplace.Equals(inputList.upInput))
+       {
+           this.keyBindingManager.setUpInput(this.actionInput);
+       }
+       else if(this.inputToReplace.Equals(inputList.downInput))
+       {
+           this.keyBindingManager.setDownInput(this.actionInput);
+       }
+       else if(this.inputToReplace.Equals(inputList.jumpInput))
+       {
+           this.keyBindingManager.setJumpInput(this.actionInput);
+       }
+       else if(this.inputToReplace.Equals(inputList.slashInput))
+       {
+           this.keyBindingManager.setSlashInput(this.actionInput);
+       }
+       else if(this.inputToReplace.Equals(inputList.projectileInput))
+       {
+           this.keyBindingManager.setProjectileInput(this.actionInput);
+       }
+       else if(this.inputToReplace.Equals(inputList.menuInput))
+       {
+           this.keyBindingManager.setMenuInput(this.actionInput);
+       }
+    }
+}
