6c529a76ba8a0a483b60ce1e4499656918ea3dca
Robert Stan <robertstan1991@gmail.com>
2021-11-11 11:40:06 +0200

    Update

--- Assets/Scripts/Traps/SpikeBallMovement.cs
+++ Assets/Scripts/Traps/SpikeBallMovement.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class SpikeBallMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]private float minRotation = -20f;
+    [SerializeField]private float maxRotation = 20f;
+    [SerializeField]private float swingMovementDelay = 0.05f;
+    private float currentRotation = 0f;
+    private bool swingingLeftDirection = true;
+    private IEnumerator swingMovementTimerInstance;
+
+    private void Awake()
     {
-        
+        if(this.swingMovementTimerInstance == null)
+        {
+            this.swingMovementTimerInstance = swingMovementTimer();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable() { StartCoroutine(this.swingMovementTimerInstance); }
+    private void OnDisable() { StopCoroutine(this.swingMovementTimerInstance); }
+
+    private IEnumerator swingMovementTimer()
     {
-        
-    }
+        this.currentRotation = this.gameObject.transform.localEulerAngles.z;
+        WaitForSeconds timerWait = new WaitForSeconds(this.swingMovementDelay);
+
+        while(true)
+        {
+            yield return timerWait;
+
+            this.currentRotation += this.swingingLeftDirection ? -1f : 1f;
+            this.gameObject.transform.localEulerAngles = new Vector3(this.gameObject.transform.localEulerAngles.x, this.gameObject.transform.localEulerAngles.y, this.currentRotation);
+            this.swingingLeftDirection = this.currentRotation.Equals(this.minRotation) ? false : this.currentRotation.Equals(this.maxRotation) ? true : this.swingingLeftDirection;
+        }
+    } 
 }
