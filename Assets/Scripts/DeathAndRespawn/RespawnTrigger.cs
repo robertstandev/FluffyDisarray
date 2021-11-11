f3d81b0d5cd15d3a65f9f3732918d0b413e6341e
Robert Stan <robertstan1991@gmail.com>
2021-11-11 10:48:19 +0200

    Updated Traps Functionality

--- Assets/Scripts/DeathAndRespawn/RespawnTrigger.cs
+++ Assets/Scripts/DeathAndRespawn/RespawnTrigger.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FallOffMapTrigger : MonoBehaviour
+public class RespawnTrigger : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
