7ddb2b4eef377613f63c7bce7fa64cedce16ddf4
Robert Stan <robertstan1991@gmail.com>
2021-11-16 10:43:12 +0200

    Update

--- Assets/Scripts/ModeMiniGames/MiniGameLava.cs
+++ Assets/Scripts/ModeMiniGames/MiniGameLava.cs
@@ -1,85 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class MiniGameLava : MonoBehaviour
 {
-    [SerializeField][Range(5,30)]private int minNrOfRounds = 5;
-    [SerializeField][Range(6,30)]private int maxNrOfRounds = 10;
     [SerializeField]private int roundDuration = 30;
-    [SerializeField]private Text displayTextComponent;
-    private int currentDuration = 0, nrOfRounds, currentRound = 0;
-    private MapCharacterManager getCharactersFromSceneScript;
-    private string aliveCharactersString;
+    private int currentDuration = 0;
+    private MiniGamesManager minigamesManagerScript;
+    private WaitForSeconds timerWait3Seconds = new WaitForSeconds(3f), timerWait1Second = new WaitForSeconds(1f);
 
-    private void Awake() { this.getCharactersFromSceneScript = FindObjectOfType<MapCharacterManager>(); }
+    private void Awake() { this.minigamesManagerScript = FindObjectOfType<MiniGamesManager>(); }
 
-    private void OnEnable()
-    {
-        StartCoroutine(roundManager());
-    }
+    private void OnEnable() { StartCoroutine(roundManager()); }
 
-    private void OnDisable()
-    {
-        StopAllCoroutines();
-        this.currentRound = 0;
-    }
+    private void OnDisable() { StopAllCoroutines(); }
 
     private IEnumerator roundManager()
     {
-        WaitForSeconds timerWait3Seconds = new WaitForSeconds(3f);
-        WaitForSeconds timerWait10Seconds = new WaitForSeconds(10f);
-
-        this.nrOfRounds = Random.Range(this.minNrOfRounds, this.maxNrOfRounds);
-
-        for(int i = 0 ; i < this.nrOfRounds; i++)
-        {
-            nextRound();
-            yield return timerWait3Seconds;
-            this.displayTextComponent.enabled = false;
-            yield return StartCoroutine(roundTimer());
-            roundFinished();
-            yield return timerWait10Seconds;
-            this.displayTextComponent.enabled = false;
-        }
-    }
-
-    private void nextRound()
-    {
-        this.currentRound += 1;
         this.currentDuration = this.roundDuration;
-        this.displayTextComponent.text = "New round is starting!";
-        this.displayTextComponent.enabled = true;
+        yield return StartCoroutine(roundTimer());
+        yield return timerWait3Seconds;
+
+        if(this.minigamesManagerScript.isActiveAndEnabled)
+        {
+            this.minigamesManagerScript.StartCoroutine(this.minigamesManagerScript.roundFinished());
+        }
+
+        this.gameObject.SetActive(false);
     }
 
     private IEnumerator roundTimer()
     {
-        WaitForSeconds timerWait = new WaitForSeconds(1f);
-
         while(this.currentDuration > 0)
         {
-            yield return timerWait;
+            yield return timerWait1Second;
             this.currentDuration -= 1;
         }
-    }
 
-    private void roundFinished()
-    {
-        checkAliveCharacters();
-        this.displayTextComponent.text = "Round winners are:\n" + this.aliveCharactersString;
-        this.displayTextComponent.enabled = true;
-    }
-
-    private void checkAliveCharacters()
-    {
-        this.aliveCharactersString = "No winners...";
-        for (int i = 0 ; i < this.getCharactersFromSceneScript.getListOfCharactersFromScene().Count; i++)
-        {
-            if(this.getCharactersFromSceneScript.getListOfCharactersFromScene()[i].activeInHierarchy)
-            {
-                this.aliveCharactersString += this.getCharactersFromSceneScript.getListOfCharactersFromScene()[i].name + " ";
-            }
-        }
     }
 }
---
+++ Assets/Scripts/ModeMiniGames/MiniGamesManager.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MiniGamesManager : MonoBehaviour
+{
+    [SerializeField]private GameObject[] miniGamesPrefabsForRounds;
+    private List<GameObject> instantiatedMiniGames = new List<GameObject>();
+    [SerializeField][Range(5,30)]private int minNrOfRounds = 5, maxNrOfRounds = 10;
+    [SerializeField]private Text displayTextComponent;
+    private int nrOfRounds, currentRound = 0;
+    private MapCharacterManager getCharactersFromSceneScript;
+    private string aliveCharactersString;
+
+    private WaitForSeconds timerWait5Seconds = new WaitForSeconds(5f), timerWait10Seconds = new WaitForSeconds(10f);
+
+    private void Awake() { this.getCharactersFromSceneScript = FindObjectOfType<MapCharacterManager>(); }
+
+    private void OnEnable() { configureAndStartGame(); }
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+        this.currentRound = 0;
+        this.instantiatedMiniGames.Clear();
+        destroyAllMiniGames();
+
+        if(this.displayTextComponent != null)
+        {
+            this.displayTextComponent.enabled = false;
+        }
+    }
+
+    private void configureAndStartGame()
+    {
+        instantiateMiniGames();
+        this.nrOfRounds = Random.Range(this.minNrOfRounds, this.maxNrOfRounds);
+        StartCoroutine(nextRound());
+    }
+
+    private IEnumerator nextRound()
+    {
+        this.currentRound += 1;
+        this.displayTextComponent.text = "New round is starting!\n" + "Good Luck!";
+        this.displayTextComponent.enabled = true;
+
+        yield return timerWait5Seconds;
+        this.displayTextComponent.enabled = false;
+        this.instantiatedMiniGames[Random.Range(0, this.instantiatedMiniGames.Count)].SetActive(true);
+    }
+
+    public IEnumerator roundFinished()
+    {
+        checkAliveCharacters();
+        this.displayTextComponent.text = "Round winners are:\n" + this.aliveCharactersString;
+        this.displayTextComponent.enabled = true;
+        yield return timerWait10Seconds;
+        this.displayTextComponent.enabled = false;
+        StartCoroutine(nextRound());     
+    }
+
+    private void checkAliveCharacters()
+    {
+        this.aliveCharactersString = "No winners...";
+        for (int i = 0 ; i < this.getCharactersFromSceneScript.getListOfCharactersFromScene().Count; i++)
+        {
+            if(this.getCharactersFromSceneScript.getListOfCharactersFromScene()[i].activeInHierarchy)
+            {
+                this.aliveCharactersString += this.getCharactersFromSceneScript.getListOfCharactersFromScene()[i].name + " ";
+            }
+        }
+    }
+
+    private void instantiateMiniGames()
+    {
+        for(int i = 0 ; i < this.miniGamesPrefabsForRounds.Length; i++)
+        {
+            this.instantiatedMiniGames.Add(Instantiate(this.miniGamesPrefabsForRounds[i] , Vector3.zero, Quaternion.identity));
+            this.instantiatedMiniGames[i].transform.parent = this.transform;
+            this.instantiatedMiniGames[i].transform.localPosition = this.miniGamesPrefabsForRounds[i].transform.position;
+            this.instantiatedMiniGames[i].transform.localEulerAngles = this.miniGamesPrefabsForRounds[i].transform.eulerAngles;
+        }
+    }
+
+    private void destroyAllMiniGames()
+    {
+        for(int i = 0 ; i < this.gameObject.transform.childCount; i++)
+        {
+            Destroy(this.gameObject.transform.GetChild(i).gameObject);
+        }
+    }
+}
