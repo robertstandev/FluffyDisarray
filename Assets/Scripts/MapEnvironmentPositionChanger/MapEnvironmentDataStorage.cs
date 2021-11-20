5a37bc3db09b1a0ee61940f98639d1fb0e45ce79
Robert Stan <robertstan1991@gmail.com>
2021-11-21 01:19:50 +0200

    Update

--- Assets/Scripts/MapEnvironmentPositionChanger/MapEnvironmentDataStorage.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class MapEnvironmentDataStorage
-{
-    private List<GameObject> spriteInstances = new List<GameObject>();
-    public List<GameObject> getSpriteInstancesList() { return this.spriteInstances; }
-}
