8b1985274076dc512e7bfcea2df3b5f60d287734
Robert Stan <robertstan1991@gmail.com>
2021-11-20 22:28:27 +0200

    Creating Advanced Environment Manager

---
+++ Assets/Scripts/MapEnvironmentPositionChanger/MapEnvironmentDataStorage.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapEnvironmentDataStorage
+{
+    private List<GameObject> spriteInstances = new List<GameObject>();
+    public List<GameObject> getSpriteInstancesList() { return this.spriteInstances; }
+}
