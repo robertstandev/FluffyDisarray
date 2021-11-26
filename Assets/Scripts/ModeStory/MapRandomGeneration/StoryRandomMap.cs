021d4f5ae6cd91cd32c44c5bb9cdbfebf32af61f
Robert Stan <robertstan1991@gmail.com>
2021-11-26 12:42:44 +0200

    Update

---
+++ Assets/Scripts/ModeStory/MapRandomGeneration/StoryRandomMap.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryRandomMap : MonoBehaviour
+{
+    [SerializeField]private GameObject test;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(test, Vector3.zero, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
