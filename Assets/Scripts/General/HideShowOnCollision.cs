5acc93d5d639b18507b4e275ccf747b534875aac
Robert Stan <robertstan1991@gmail.com>
2022-04-07 18:27:39 +0300

    Update

---
+++ Assets/Scripts/General/HideShowOnCollision.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideShowOnCollision : MonoBehaviour
+{
+    [SerializeField]private GameObject[] gameObjectToHideShow;
+    private MapCharacterManager getCharactersFromSceneScript;
+
+    private void Awake() { this.getCharactersFromSceneScript = FindObjectOfType<MapCharacterManager>(); }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(!this.getCharactersFromSceneScript.isCollidedObjectInList(other.gameObject)) { return; }
+        for(int i = 0 ; i < this.gameObjectToHideShow.Length ; i++)
+        {
+            this.gameObjectToHideShow[i].SetActive(!this.gameObjectToHideShow[i].activeInHierarchy);
+        }
+    }
+}
