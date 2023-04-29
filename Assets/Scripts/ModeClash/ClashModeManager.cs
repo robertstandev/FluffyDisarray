9e5cfa011468386c0b6484d61d8f2e840f6ccd99
Robert Stan <robertstan1991@gmail.com>
2023-04-29 09:59:39 +0300

    Update ClashModeManager.cs

--- Assets/Scripts/ModeClash/ClashModeManager.cs
+++ Assets/Scripts/ModeClash/ClashModeManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ClashModeManager : MonoBehaviour
 {
+    //Aici se va tine cont de scor (de cate ori a murit fiecare si daca mai e timp valabil sau s-a terminat runda)
+
     // Start is called before the first frame update
     void Start()
     {
