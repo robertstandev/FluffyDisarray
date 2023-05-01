79e4b5853313c20839cedc726ad7a1220e547c51
Robert Stan <robertstan1991@gmail.com>
2023-05-01 13:47:29 +0300

    Update QuestManager.cs

--- Assets/Scripts/ModeStory/QuestManager.cs
+++ Assets/Scripts/ModeStory/QuestManager.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class QuestManager : MonoBehaviour
 {
+
+    //Va tine cont de toate questurile care sunt de facut si care au fost deja facute
+
+    //Se va face cu 2 array si adaugat din UI din Editor
+    //1 va fi pentru descrierea questului
+    //2 va fi in care se va zice ce gameobject trebuie sa dispara
+    //(ex daca trebuie sa omori un boss cand ala va fi spriteRenderul.enabled false inseamna ca a fost omorat ,
+    //la fel daca trebuie sa ia un item ceva de undeva cand devine SpriteRenderul.enabled false atunci a fost luat
+    //(daca folosesc alta metoda gen gameobject.visible false atunci trebuie sa fiu atent ca questul sa se activeze doar dupa acel gameobject devine visible.true))
+    
+
     // Start is called before the first frame update
     void Start()
     {
