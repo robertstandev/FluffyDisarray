72f35d8e3d1ee8c7405b398e104be3ac56b4d0a2
Robert Stan <robertstan1991@gmail.com>
2023-05-04 06:43:53 +0300

    Update SpikeBallMovement.cs

--- Assets/Scripts/Traps/SpikeBallMovement.cs
+++ Assets/Scripts/Traps/SpikeBallMovement.cs
@@ -9,27 +9,29 @@ public class SpikeBallMovement : MonoBehaviour
     [SerializeField]private float swingMovementDelay = 0.05f;
     private float currentRotation = 0f;
     private bool swingingLeftDirection = true;
+    private Vector3 gameObjectRotation;
+    private WaitForSeconds timerWait;
 
     private void OnEnable()
     {
         this.swingingLeftDirection = true;
+        this.timerWait = new WaitForSeconds(this.swingMovementDelay);
         StartCoroutine(swingMovementTimer());
     }
     private void OnDisable() { StopAllCoroutines(); }
 
     private IEnumerator swingMovementTimer()
     {
-        WaitForSeconds timerWait = new WaitForSeconds(this.swingMovementDelay);
-        Vector3 gameObjectRotation = this.gameObject.transform.localEulerAngles;
-        this.currentRotation = gameObjectRotation.z;
+        this.gameObjectRotation = this.gameObject.transform.localEulerAngles;
+        this.currentRotation = this.gameObjectRotation.z;
 
         while(true)
         {
-            yield return timerWait;
+            yield return this.timerWait;
 
             this.currentRotation += this.swingingLeftDirection ? -1f : 1f;
-            gameObjectRotation.z = this.currentRotation;
-            this.gameObject.transform.localEulerAngles = gameObjectRotation;
+            this.gameObjectRotation.z = this.currentRotation;
+            this.gameObject.transform.localEulerAngles = this.gameObjectRotation;
             this.swingingLeftDirection = this.currentRotation <= this.minRotation ? false : this.currentRotation >= this.maxRotation ? true : this.swingingLeftDirection;
         }
     } 
